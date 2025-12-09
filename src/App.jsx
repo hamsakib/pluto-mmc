9210bb056c3c2f53ee875e8efbf3ee857aee3cac
hamsakib <hamsakib03@gmail.com>
2025-12-09 12:59:41 +0600

    Initial website for Pluto MMC

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header>...</header>
+      <main>
+        <section id="hero">...</section>
+        <section id="services">...</section>
+        <section id="portfolio">...</section>
+        <section id="about">...</section>
+        <section id="contact">...</section>
+      </main>
+      <footer>...</footer>
+    </>
+  );
+}
+
+export default App;
